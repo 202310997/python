e34ac780286a472e5d41a052a50f8297dea0b3b1
202310997 <128023167+202310997@users.noreply.github.com>
2023-04-21 16:27:19 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 개념 확인 과제
+
+for n in range(1, 6) :
+    for i in range(2, 6) :
+        print(f'{i} x {n} = {i * n}', end='\t')
+    print()
+    
+for n in range(6, 10) :
+    for i in range(2, 6) :
+        print(f'{i} x {n} = {i * n}', end='\t')
+    print()
+
+print()
+
+for n in range(1, 6) :
+    for i in range(6, 10) :
+        print(f'{i} x {n} = {i * n}', end='\t')
+    print()
+    
+for n in range(6, 10) :
+    for i in range(6, 10) :
+        print(f'{i} x {n} = {i * n}', end='\t')
+    print()
+
+
+# In[ ]:
+
+
+
+
